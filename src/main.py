5deceadbff1a2973557f21d6119c72d8bb7b7b6a
Berke Serçe <berkeserce@users.noreply.github.com>
2025-03-16 17:13:47 +0300

    Regulation

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import ctypes
+import subprocess
 
 def is_admin():
     try:
@@ -18,12 +19,19 @@ def run_as_admin():
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 from src.controllers.tor_controller import TorController
 
 def main():
     run_as_admin()
     
     app = QApplication(sys.argv)
+    
+    # Uygulama simgesini ayarla
+    icon_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ui', 'logo.ico')
+    if os.path.exists(icon_path):
+        app.setWindowIcon(QIcon(icon_path))
+    
     window = TorController()
     window.show()
     sys.exit(app.exec())
